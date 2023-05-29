3c10ee1fc3ddc45985b836f69e28e68f3439ac47
xBlaxk <blaxkmaster@gmail.com>
2023-05-28 23:31:21 -0500

    feat: add conditional rendering

--- src/components/pokeGallery.vue
+++ src/components/pokeGallery.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex w-full flex-wrap items-center justify-center gap-4">
+    <div v-if="images" class="flex w-full flex-wrap items-center justify-center gap-4">
         <h2 class="w-full text-xl font-bold uppercase">{{ title }}</h2>
         <div v-for="(sprite, spriteKey) in images" :key="spriteKey">
-            <div v-if="typeof sprite === 'string'" class="flex flex-col p-2 shadow-lg shadow-slate-300 rounded-md">
+            <div v-if="typeof sprite === 'string'" class="flex flex-col rounded-md p-2 shadow-lg shadow-slate-300">
                 <label class="text-bold text-xl capitalize">{{ splitAndJoinText(spriteKey) }}</label>
                 <div class="h-48 w-48 object-contain">
                     <img :src="sprite" :alt="spriteKey" class="h-full w-full" />
@@ -29,4 +29,3 @@ const splitAndJoinText = (text) => {
     return text.split(textSplitRegEx).join(' ')
 }
 </script>
-
