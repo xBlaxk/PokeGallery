a6d2dface5fa5daffa9a5c3a77c5ce4a153679d7
xBlaxk <blaxkmaster@gmail.com>
2023-05-28 20:21:03 -0500

    feat: remove unsued lines

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import httpMethods from './plugins/api/rest'
-// import VueAxios from 'vue-axios'
-console.log(await httpMethods.GET({ url: 'https://pokeapi.co/api/v2/pokemon/ditto' }))
 
 createApp(App).mount('#app')
