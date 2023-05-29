352b0801b4bce92ca0e9f3be9469728b6897e138
xBlaxk <blaxkmaster@gmail.com>
2023-05-28 20:32:05 -0500

    feat: create axios implementation file

--- src/plugins/api/rest/index.js
+++ src/plugins/api/rest/index.js
@@ -2,25 +2,21 @@ import axios from 'axios'
 
 const httpMethods = {
     GET: async (input) => {
-        const response = await axios.get(input.url)
+        const response = await axios.get(input.url, input.config)
         return response
     },
     POST: async (input) => {
-        const response = await axios.post(input.url)
+        const response = await axios.post(input.url, input.data, input.config)
         return response
     },
     PUT: async (input) => {
-        const response = await axios.put(input.url)
+        const response = await axios.put(input.url, input.data, input.config)
         return response
     },
     DELETE: async (input) => {
-        const response = await axios.delete(input.url)
+        const response = await axios.delete(input.url, input.data, input.config)
         return response
     },
 }
-
-function config() {}
-function validate() {}
-function setup() {}
 
 export default httpMethods
