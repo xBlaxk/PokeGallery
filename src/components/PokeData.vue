eda04f6c9dcbd0b29f1e8174931d63e802e67595
xBlaxk <blaxkmaster@gmail.com>
2023-05-28 21:08:22 -0500

    feat: format code and update link

--- src/components/PokeData.vue
+++ src/components/PokeData.vue
@@ -2,11 +2,11 @@
     <div class="mx-auto my-4 flex w-11/12 flex-wrap items-center justify-center rounded-md bg-slate-100 shadow">
         <h1 class="w-full p-3 text-2xl font-bold uppercase">{{ pikachu?.name }}</h1>
         <div class="flex flex-wrap gap-4 p-4">
-            <div class="flex flex-wrap items-center justify-center gap-4 w-full p-8 shadow-md rounded-md">
+            <div class="flex w-full flex-wrap items-center justify-center gap-4 rounded-md p-8 shadow-md">
                 <poke-gallery :title="'Original'" :images="pikachu.sprites"></poke-gallery>
             </div>
-            <div class="flex flex-wrap items-center justify-center gap-4 w-full p-8 shadow-md rounded-md">
-                <h2 class="w-full text-xl text-left font-bold uppercase">Other</h2>
+            <div class="flex w-full flex-wrap items-center justify-center gap-4 rounded-md p-8 shadow-md">
+                <h2 class="w-full text-left text-xl font-bold uppercase">Other</h2>
                 <div v-for="(otherVersions, otherVersionsKey) in pikachu.sprites.other" :key="otherVersionsKey">
                     <poke-gallery
                         :title="splitAndJoinText(otherVersionsKey)"
@@ -14,12 +14,22 @@
                     ></poke-gallery>
                 </div>
             </div>
-            <div class="flex flex-wrap items-center justify-center gap-4 w-full p-8 shadow-md rounded-md">
-                <h2 class="w-full text-xl text-left font-bold uppercase">Generations</h2>
-                <div v-for="(generationGame, generationKey) in pikachu.sprites.versions" :key="generationKey">
-                    <h3 class="text-xl font-bold uppercase">{{ splitAndJoinText(generationKey) }}</h3>
-                    <div v-for="(game, gameKey) in generationGame" :key="gameKey">
-                        <poke-gallery :title="splitAndJoinText(gameKey)" :images="game"></poke-gallery>
+            <div class="flex w-full flex-wrap items-center justify-center gap-4 rounded-md p-8 shadow-md">
+                <h2 class="w-full text-left text-xl font-bold uppercase">Generations</h2>
+                <div
+                    v-for="(generationGame, generationKey) in pikachu.sprites.versions"
+                    :key="generationKey"
+                    class="flex flex-wrap gap-4"
+                >
+                    <div class="flex flex-wrap gap-4 rounded-md p-4 shadow-md">
+                        <h3 class="w-full text-xl font-bold uppercase">{{ splitAndJoinText(generationKey) }}</h3>
+                        <div
+                            v-for="(game, gameKey) in generationGame"
+                            :key="gameKey"
+                            class="flex w-full flex-wrap gap-4 rounded-md p-4 shadow-md"
+                        >
+                            <poke-gallery :title="splitAndJoinText(gameKey)" :images="game"></poke-gallery>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,24 +1,39 @@
 <template>
-    <div class="bg-slate-100 flex wrap flex-col gap-4 justify-center items-center w-1/2 p-4 mx-auto my-8 border rounded-lg shadow">
+    <div
+        class="wrap mx-auto my-8 flex w-11/12 max-w-3xl flex-col items-center justify-center gap-8 rounded-lg border bg-slate-100 p-8 shadow"
+    >
         <h1 class="text-3xl font-bold">Search</h1>
-        <form class="flex flex-col justify-center items-center gap-2 w-full" @submit.prevent="searchPokemon(searchInput)">
-            <input v-model="searchInput" class="shadow rounded-lg w-full h-12 p-4 focus:outline-none focus:shadow-blue-300 focus:shadow-lg" type="text" placeholder="Search for a Pokemon">
-            <button type="submit" class="w-2/4 bg-blue-400 text-md font-bold text-white p-1 shadow rounded-md hover:scale-105 hover:shadow-lg">Submit</button>
+        <form
+            class="flex w-full flex-col items-center justify-center gap-8"
+            @submit.prevent="searchPokemon(searchInput)"
+        >
+            <input
+                v-model="searchInput"
+                class="h-12 w-full rounded-lg p-4 shadow focus:shadow-lg focus:shadow-blue-300 focus:outline-none"
+                type="text"
+                placeholder="Search for a Pokemon"
+            />
+            <button
+                type="submit"
+                class="text-md w-2/4 rounded-md bg-blue-400 p-3 font-bold text-white shadow hover:scale-105 hover:shadow-lg"
+            >
+                Submit
+            </button>
         </form>
     </div>
 </template>
 <script setup>
 import { onMounted, ref } from 'vue'
-import axios from 'axios';
+import axios from 'axios'
 
 defineProps({
     msg: String,
 })
 const emit = defineEmits(['updateData'])
 
-const searchInput = ref("pikachu")
+const searchInput = ref('pikachu')
 const pokemonData = ref({})
-const defaultPokemon = ref("pikachu")
+const defaultPokemon = ref('pikachu')
 
 async function searchPokemon(pokemonName) {
     const reponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
