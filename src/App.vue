d5dccbc2232de00a9bf742229eb95d01ce52664c
xBlaxk <blaxkmaster@gmail.com>
2023-05-28 22:47:43 -0500

    fix: event doesn't trigger the fucntion updateData

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
     <div class="flex min-h-screen flex-col text-center selection:bg-green-100">
         <NavBar></NavBar>
         <div class="my-8 grow">
-            <SearchBox />
-            <PokeData :pokemonData="pokemonData" @updateData="count++"></PokeData>
+            <SearchBox @updateData="updateData" />
+            <PokeData :pokemonData="pokemonData"></PokeData>
         </div>
         <Footer class="mt-auto"></Footer>
     </div>
@@ -14,12 +14,11 @@ import NavBar from '@/components/NavBar.vue'
 import PokeData from '@/components/PokeData.vue'
 import Footer from '@/components/Footer.vue'
 import { ref } from 'vue'
+// import pokemonData from '@/assets/pikachu.json'
 
 const pokemonData = ref({})
-const count = ref(0)
 const updateData = (data) => {
-    console.log('data')
+    pokemonData.value = data
 }
-
 </script>
 <style></style>
