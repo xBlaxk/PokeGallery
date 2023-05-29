e0934af2ba5608cf8e7771d8682fb3cc2ef9447c
xBlaxk <blaxkmaster@gmail.com>
2023-05-28 20:21:40 -0500

    feat: add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="bg-white shadow dark:bg-gray-800">
+        <div class="mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span class="text-sm text-gray-500 dark:text-gray-400 sm:text-center"
+                >© 2023 <a href="https://flowbite.com/" class="hover:underline">PokeGallery</a>. All Rights Reserved.
+            </span>
+        </div>
+    </footer>
+</template>
